7e6c49a5c0101e4077be31ad81b1caad53a47b2e
Ali Ganbarov <Ali Ganbarov>
2016-09-14 20:10:58 +0400

    Pop-up bug fixed, Search Bar added

--- assets/js/ali.js
+++ assets/js/ali.js
@@ -20,36 +20,8 @@ $('.thumbType a').mouseleave(function(){
 })
 
 /* *************************************************************************** */
+
 /* CONFIG FOR POP-UP WINDOW(.smallInfo) */
-
-function SmallInfoConfig(bigId, smallId) {
-	var bottomOfThumb = $(bigId).offset().top + 230 + 5- 55*2; //230 is height of .thumb
-	var rightOfThumb = $(bigId).offset().left + 215;
-
-	//first decide to pop on bottom or top
-	var freeBottomSpace = $(window).height() - bottomOfThumb;
-	var freeRightSpace = $(window).width() - rightOfThumb;
-
-	var leftPos = $(bigId).offset().left + $(bigId).width() / 2 + 40;
-
-	//check if no space to put pop-up on right of thumb
-	if (freeRightSpace < 300) {
-		//put pop-up on left side
-		var leftPos = $(bigId).offset().left - $(bigId).width() + 40;
-	}
-
-	if (freeBottomSpace < 190) {
-		bottomOfThumb -= 230;
-	}
-
-	//SET POSITIONS
-	$(smallId).css({
-		'top' 	: bottomOfThumb + 'px',
-		'left' 	: leftPos + 'px'
-	})
-	$(smallId).removeClass('hide');
-}
-
 if ($(window).width() > 1005) {
 	$('.user_name').mouseenter(function(){
 		parent = $(this).parent().parent().parent();
@@ -58,21 +30,68 @@ if ($(window).width() > 1005) {
 		SmallInfoConfig(bigId, smallId);
 	})
 
+	delay = 200;
+	$('.user_name').mouseleave(function(){
+		parent = $(this).parent().parent().parent();
+		bigId = '#' + $(parent).attr('id') + '';
+		smallId = '#user_small_' + $(parent).attr('id');
+		setTimeout(function() {
+			$(smallId).addClass('hide');
+		}, delay);			
+	})
+
+	$('.smallInfo').mouseenter(function(){
+		$(this).removeClass('hide');
+		$(smallId).addClass('show');
+	})
+
 	$('.smallInfo').mouseleave(function() {
-		// parent = $(this).parent().parent().parent();
-		// bigId = '#' + $(parent).attr('id') + '';
-		// smallId = '#user_small_' + $(parent).attr('id');
-		// $(smallId).addClass('hide');
 		$(this).addClass('hide');
+		$(this).removeClass('show');
 	})
 }
+
+/* ************** 		SETTINGS FOR SEARCH BAR BIG SCREEN			************ */
+//HIDE AT PAGE LOAD
+$('.searchExpanded').hide();
+
+//show search bar
+$('.searchIcon').click(function(){
+	$(this).addClass('noShow');
+	$('.searchExpanded').show();
+	$('.searchBar').focus();
+})
+
+//hide search bar
+$('.searchExpanded').focusout(function(){
+	$('.searchBar').val('');
+	$(this).hide();
+	$('.searchIcon').removeClass('noShow');
+})
+
+/* ************** 		SETTINGS FOR SEARCH BAR SMALL SCREEN			************ */
+//HIDE AT PAGE LOAD
+$('.searchExpandedSmall').hide();
+
+//show search bar
+$('.searchIconSmall').click(function(){
+	$(this).addClass('noShow');
+	$('.searchExpandedSmall').show();
+	$('.searchBarSmall').focus();
+})
+
+//hide search bar
+$('.searchExpandedSmall').focusout(function(){
+	$('.searchBarSmall').val('');
+	$(this).hide();
+	$('.searchIconSmall').removeClass('noShow');
+})
 
 /* *************************************************************************** */
 //SETTING HEIGHT OF SIDEBAR EQUAL TO WINDOW HEIGHT
 $('.sidebar').css({
 	height: $(window).height() + 'px'
 })
-
 
 //PUTTING SIDEBAR ON LEFT
 var barClicked = false;
@@ -96,6 +115,14 @@ $('.fa-bars').on('click', function(){
 })
 
 /* ************** FUNCTIONS ************** */
+
+//MANIPULATE POP-UP HIDE CONDITION
+function PopUpHide(smallId) {
+	setTimeout(function() {
+		$(smallId).addClass('hide');
+
+	}, 2000);	
+}
 
 //HIDE/SHOW SIDEBAR
 function HideSideBar() {
@@ -136,3 +163,32 @@ function ChooseNumberOfPanels() {
 		return 1;
 	}
 }
+
+function SmallInfoConfig(bigId, smallId) {
+	var bottomOfThumb = $(bigId).offset().top + 230 + 5- 55*2; //230 is height of .thumb
+	var rightOfThumb = $(bigId).offset().left + 215;
+
+	//first decide to pop on bottom or top
+	var freeBottomSpace = $(window).height() - bottomOfThumb;
+	var freeRightSpace = $(window).width() - rightOfThumb;
+
+	var leftPos = $(bigId).offset().left + $(bigId).width() / 2 + 40;
+
+	//check if no space to put pop-up on right of thumb
+	if (freeRightSpace < 300) {
+		//put pop-up on left side
+		var leftPos = $(bigId).offset().left - $(bigId).width() + 40;
+	}
+
+	if (freeBottomSpace < 190) {
+		bottomOfThumb -= 230;
+	}
+
+	//SET POSITIONS
+	$(smallId).css({
+		'top' 	: bottomOfThumb + 'px',
+		'left' 	: leftPos + 'px'
+	})
+	$(smallId).removeClass('hide');
+}
+
